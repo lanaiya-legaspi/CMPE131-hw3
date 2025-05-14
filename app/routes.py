02a9460a6b61ae53b77c8a265c4673e786c48136
Lanaiya Wu <legaspi.lanaiya@gmail.com>
2025-05-13 18:01:19 -0700

    added functionality to add recipes page. submission rerouting works, but data is not added to db

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,36 @@
 from app import myapp_obj
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from app.forms import LoginForm, RecipeForm
-from app.models import User #, RecipeList
+from app.models import User, Recipe
 from app import db
+from datetime import datetime
 
-@myapp_obj.route("/")
-@myapp_obj.route("/recipes")
+@myapp_obj.route("/", methods=["GET","POST"])
+@myapp_obj.route("/recipes", methods=["GET","POST"])
 def main():
 	username = "user04732"
-#	recipes = RecipeList()
-	return render_template("recipes.html", username=username)
+	recipes = Recipe.query.all()
+	return render_template("recipes.html", username=username, recipes=recipes)
 
 
-@myapp_obj.route("/recipes/new")
+@myapp_obj.route("/add-recipe", methods=["GET", "POST"])
 def new_recipe():
 	form = RecipeForm()
-	if form.validate_on_submit():
-		return redirect("/")
+
+	if request.method == "POST":
+		title = request.form['title']
+		desc = request.form['description']
+		ings = request.form['ingredients']
+		insns = request.form['instructions']
+		tmstmp = str(datetime.now().strftime("%m-%d-%Y"))
+		recipe = Recipe(title=title, description=desc, ingredients=ings, instructions=insns, created=tmstmp)
+		db.session.add(recipe)
+		db.session.commit()
+		print("New recipe added!")
+		return redirect(url_for("recipes"))
 	else:
 		print("MOOO MOOO")
-	return render_template("new-recipes.html", form=form)
+	return render_template("add-recipe.html", form=form)
 
 
 @myapp_obj.route("/accounts")
@@ -35,3 +46,12 @@ def login():
 	else:
 		print("MOOOO MOOO")
 	return render_template("login.html", form=form)
+
+@myapp_obj.route("/register")
+def register():
+	form = LoginForm()
+	if form.validate_on_submit():
+		return redirect("/")
+	else:
+		print("MOOO MOOO")
+	return render_template("register.html", form=form)
